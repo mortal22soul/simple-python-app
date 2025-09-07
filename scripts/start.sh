dec6535948cbb2c5b689b9c6be659f87261e5834
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2025-09-07 13:24:23 +0530

    added sonarqube

--- scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "Starting new container..."
-docker pull aryanmehesare/simple-python-app:latest
-docker run -d --name simple-python-app -p 5000:5000 aryanmehesare/simple-python-app:latest
--- scripts/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "Stopping old container..."
-docker stop simple-python-app || true
-docker rm simple-python-app || true
